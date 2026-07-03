ec75e051311e1d9872542ff9b00757a923a0dc09
roey2006 <roey.feldman06@gmail.com>
2026-07-03 19:41:54 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import base64
+
+app = Flask(__name__)
+
+current_image = None
+new_message = False
+
+@app.route('/send', methods=['POST'])
+def send_image():
+    global current_image, new_message
+    data = request.get_json()
+    current_image = data['image']
+    new_message = True
+    return jsonify({'status': 'ok'})
+
+@app.route('/check', methods=['GET'])
+def check_message():
+    global new_message
+    if new_message:
+        return jsonify({'new_message': True})
+    return jsonify({'new_message': False})
+
+@app.route('/image', methods=['GET'])
+def get_image():
+    global new_message
+    new_message = False
+    return jsonify({'image': current_image})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
